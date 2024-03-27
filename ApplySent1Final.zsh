5f971ddfe068f802ca4a70450e8039ec1ea775c9
jason-ari-tg <159946855+jason-ari-tg@users.noreply.github.com>
2024-03-27 08:50:25 -0400

    Add files via upload

---
+++ ApplySent1Final.zsh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+# Define the URL of the script on GitHub
+SCRIPT_URL="https://raw.githubusercontent.com/jason-ari-tg/sent1/main/ApplySentToken-Output.zsh"
+
+# Download the script, execute it, and output the results
+curl -sSL "$SCRIPT_URL" | zsh
